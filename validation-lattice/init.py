9327e6361510d4f81c27ebbc8410b163c789a91a
chrisjones4 <chrisjones4@u.boisestate.edu>
2023-10-02 13:14:08 -0600

    add proj for lattice sims

---
+++ validation-lattice/init.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+"""Initialize the project's data space.
+
+Iterates over all defined state points and initializes
+the associated job workspace directories.
+The result of running this file is the creation of a signac workspace:
+    - signac.rc file containing the project name
+    - signac_statepoints.json summary for the entire workspace
+    - workspace/ directory that contains a sub-directory of every individual statepoint
+    - signac_statepoints.json within each individual statepoint sub-directory.
+
+"""
+
+import signac
+import flow
+import logging
+from collections import OrderedDict
+from itertools import product
+
+
+def get_parameters():
+    ''''''
+    parameters = OrderedDict()
+    parameters["num_mols"] = [50]
+    parameters["lengths"] = [25]
+    parameters["density"] = [1.43]
+    parameters["remove_hydrogens"] = [
+            True,
+            #False
+    ]
+    parameters["remove_charges"] = [
+            True,
+            #False
+    ]
+    parameters["sigma_scale"] = [0.955]
+    parameters["kT"] = [1.40]
+    parameters["pressure"] = [
+            0.002332,
+            0.0013933,
+    ]
+    parameters["n_steps"] = [
+            2e7
+    ]
+    parameters["r_cut"] = [2.5]
+    parameters["tau_kT"] = [100]
+    parameters["tau_pressure"] = [800]
+    parameters["gamma"] = [0]
+    parameters["gsd_write_freq"] = [1e5]
+    parameters["log_write_freq"] = [1e4]
+    parameters["sim_seed"] = [42]
+    return list(parameters.keys()), list(product(*parameters.values()))
+
+
+def main():
+    project = signac.init_project() # Set the signac project name
+    param_names, param_combinations = get_parameters()
+    # Create the generate jobs
+    for params in param_combinations:
+        statepoint = dict(zip(param_names, params))
+        job = project.open_job(statepoint)
+        job.init()
+        job.doc.setdefault("validate_tg_done", False)
+        job.doc.setdefault("validate_lattice_done", False)
+        job.doc.setdefault("volume_sampled", False)
+        job.doc.setdefault("equilibrated", False)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    main()
---
+++ validation-lattice/project.py
@@ -0,0 +1,229 @@
+"""Define the project's workflow logic and operation functions.
+
+Execute this script directly from the command line, to view your project's
+status, execute operations and submit them to a cluster. See also:
+
+    $ python src/project.py --help
+"""
+import signac
+from flow import FlowProject, directives
+from flow.environment import DefaultSlurmEnvironment
+import os
+
+
+class MyProject(FlowProject):
+    pass
+
+
+class Borah(DefaultSlurmEnvironment):
+    hostname_pattern = "borah"
+    template = "borah.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="shortgpu",
+            help="Specify the partition to submit to."
+        )
+
+
+class R2(DefaultSlurmEnvironment):
+    hostname_pattern = "r2"
+    template = "r2.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="shortgpuq",
+            help="Specify the partition to submit to."
+        )
+
+
+class Fry(DefaultSlurmEnvironment):
+    hostname_pattern = "fry"
+    template = "fry.sh"
+
+    @classmethod
+    def add_args(cls, parser):
+        parser.add_argument(
+            "--partition",
+            default="batch",
+            help="Specify the partition to submit to."
+        )
+
+# Definition of project-related labels (classification)
+
+@MyProject.label
+def validate_lattice_done(job):
+    return job.doc.validate_lattice_done
+
+
+@MyProject.label
+def sample_volume_done(job):
+    return job.doc.volume_sampled
+
+
+@MyProject.post(validate_lattice_done)
+@MyProject.operation(
+        directives={"ngpu": 1, "executable": "python -u"},
+        name="validate-lattice"
+)
+
+
+@MyProject.post(validate_lattice_done)
+@MyProject.operation(
+        directives={"ngpu": 1, "executable": "python -u"},
+        name="validate-lattice"
+)
+def run_validate_lattice(job):
+    """Run a lattice simulation; equilibrate in NPT"""
+    import unyt
+    from unyt import Unit
+    import hoomd_organics
+    from hoomd_organics.base.system import Pack
+    from hoomd_organics.library import PPS, OPLS_AA_PPS
+    from hoomd_organics.base.simulation import Simulation
+    with job:
+        print("------------------------------------")
+        print("JOB ID NUMBER:")
+        print(job.id)
+        print("------------------------------------")
+
+        pps = PPS(num_mols=job.sp.num_mols, lengths=job.sp.lengths)
+
+        system = Lattice(
+                molecules=[pps],
+                density=1.42,
+                x=0.867,
+                y=0.561,
+                n=5
+        )
+        system.apply_forcefield(
+            r_cut=job.sp.r_cut,
+            auto_scale=True,
+            scale_charges=True,
+            remove_hydrogens=job.sp.remove_hydrogens,
+            remove_charges=job.sp.remove_charges,
+            force_field=OPLS_AA_PPS()
+        )
+        # Store reference units and values
+        job.doc.ref_mass = system.reference_mass.to("amu").value
+        job.doc.ref_mass_units = "amu"
+        job.doc.ref_energy = system.reference_energy.to("kJ/mol").value
+        job.doc.ref_energy_units = "kJ/mol"
+        job.doc.ref_length = (
+                system.reference_length.to("nm").value * job.sp.sigma_scale
+        )
+        job.doc.ref_length_units = "nm"
+        if job.sp.remove_hydrogens:
+            dt = 0.0003
+        else:
+            dt = 0.0001
+        job.doc.dt = dt
+        # Set up Simulation obj
+        gsd_path = job.fn("trajectory.gsd")
+        log_path = job.fn("log.txt")
+
+        sim = Simulation.from_system(
+                system,
+                gsd_write_freq=job.sp.gsd_write_freq,
+                gsd_file_name=gsd_path,
+                log_write_freq=job.sp.log_write_freq,
+                log_file_name=log_path,
+                dt=job.doc.dt,
+                seed=job.sp.sim_seed,
+        )
+        sim.pickle_forcefield(job.fn("forcefield.pickle"))
+
+        # Store more unit information in job doc
+        target_box = system.target_box / job.doc.ref_length
+        tau_kT = job.doc.dt * job.sp.tau_kT
+        tau_pressure = job.doc.dt * job.sp.tau_pressure
+        job.doc.tau_kT = tau_kT
+        job.doc.tau_pressure = tau_pressure
+        job.doc.real_time_step = sim.real_timestep.to("fs").value
+        job.doc.real_time_units = "fs"
+
+        print("Running initial relaxation step.")
+        sim.run_NVT(n_steps=5e6, kT=1.0, tau_kt=tau_kT)
+
+        print("Annealing to final temperature...")
+        shrink_kT_ramp = sim.temperature_ramp(
+                n_steps=5e6,
+                kT_start=1.0,
+                kT_final=job.sp.kT
+        )
+
+        print("Running NVT at final temperature.")
+        sim.run_NVT(n_steps=5e6, kT=job.sp.kT, tau_kt=tau_kT)
+
+        print("Running NPT simulation.")
+        sim.run_NPT(
+            n_steps=job.sp.n_steps,
+            kT=job.sp.kT,
+            pressure=job.sp.pressure,
+            tau_kt=tau_kT,
+            tau_pressure=job.doc.tau_pressure,
+            gamma=job.sp.gamma
+        )
+        sim.save_restart_gsd(job.fn("restart.gsd"))
+        print("Simulation finished.")
+
+@MyProject.pre(validate_tg_done)
+@MyProject.post(sample_volume_done)
+@MyProject.operation(
+        directives={"ngpu": 1, "executable": "python -u"}, name="sample-npt"
+)
+def sample_npt(job):
+    from cmeutils.sampling import is_equilibrated, equil_sample
+    import numpy as np
+    import unyt as u
+    from unyt import Unit
+
+    with job:
+        print("JOB ID NUMBER:")
+        print(job.id)
+        print("------------------------------------")
+        data = np.genfromtxt(job.fn("log.txt"), names=True)
+        volume = data["mdcomputeThermodynamicQuantitiesvolume"]
+        pe = data["mdcomputeThermodynamicQuantitiespotential_energy"]
+        num_points = len(volume)
+        volume_eq = is_equilibrated(
+                volume[num_points//2:],
+                threshold_neff=100,
+                threshold_fraction=0.10
+        )[0]
+        pe_eq = is_equilibrated(
+                pe[num_points//2:],
+                threshold_neff=100,
+                threshold_fraction=0.10
+        )[0]
+        if all([volume_eq, pe_eq]):
+            job.doc.equilibrated = True
+            uncorr_sample, uncorr_indices, prod_start, Neff = equil_sample(
+                    volume[num_points//2:],
+                    threshold_fraciton=0.10,
+                    threshold_neff=100
+            )
+            vol_nm = uncorr_sample * job.doc.ref_length * Unit("nm**3")
+            vol_cm = vol_nm.to("cm**3")
+            np.savetxt(job.fn("vol_sample_indices.txt"), uncorr_indices)
+            np.savetxt(job.fn("volume_cc.txt"), vol_cm.value)
+            job.doc.avg_vol = np.mean(vol_cm).value
+            job.doc.vol_std = np.std(vol_cm).value
+
+            with gsd.hoomd.open(job.fn("restart.gsd")) as traj:
+                snap = traj[0]
+                reduced_mass = sum(snap.particles.mass)
+                mass_amu = (reduced_mass * job.doc.ref_mass) * Unit("amu")
+                mass_g = mass_amu.to("g")
+                job.doc.mass_g = mass_g.value
+
+            job.doc.avg_density = job.doc.mass_g / job.doc.avg_vol
+            job.doc.density_std = job.doc.mass_g / job.doc.vol_std
+            job.doc.volume_sampled = True
+
+if __name__ == "__main__":
+    MyProject().main()
