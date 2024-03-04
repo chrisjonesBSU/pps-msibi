36b820e87b50f6eefcdbf06b39273a478c1039a1
marjanalbouye <marjan.albouye73@gmail.com>
2024-03-04 13:46:57 -0700

    change state cg file name

--- msibi-flow/bond-flow/init.py
+++ msibi-flow/bond-flow/init.py
@@ -38,7 +38,7 @@ def get_parameters(ordered_dict=OrderedDict()):
         "/home/erjank_project/PPS-MSIBI/pps-msibi/training-runs/single-chains"
     ]
     parameters["single_chain_job_id"] = ["29a7f0d216700e7c8534b8c11140ba06"]
-    parameters["cg_file_name"] = ["cg-trajectory0.gsd"]
+    parameters["cg_file_name"] = ["target_1monomer_per_bead.gsd"]
     parameters["states"] = [
         [
             {"name": "A",
